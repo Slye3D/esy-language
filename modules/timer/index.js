b0fbe5c027df56eabbcf5a0a8288d70eb7bd5fb5
Alireza Ghadimi <alirezaghadimy@gmail.com>
2017-06-15 17:50:20 +0430

    First module :)

---
+++ modules/timer/index.js
@@ -0,0 +1,38 @@
+/**
+ *    _____ __
+ *   / ___// /_  _____
+ *   \__ \/ / / / / _ \
+ *  ___/ / / /_/ /  __/
+ * /____/_/\__, /\___/
+ *       /____/
+ *       Copyright 2017 Slye Development Team. All Rights Reserved.
+ *       Licence: MIT License
+ */
+
+/**
+ * Support for interval and timeout property
+ * @param esy
+ */
+function timer(esy){
+	esy.block(/^(interval|timeout)(?:\s+(?=\d+)(\d+))?(?:\s*<(.*)>)?(?:\s*\(([\w,]+)\))?$/ig, (matches, block, parent, offset) => {
+		var func        = matches[1].toLowerCase() == 'timeout' ? 'setTimeout' : 'setInterval';
+		var callback    = esy.compile(block.body);
+		var delay       = matches[2] || 0;
+		var pass_args   = matches[3];
+		var func_args   = matches[4];
+		var re  = func + '(function(';
+		if(func_args)
+			re  += func_args;
+		re += '){' + callback + '}, ' + delay;
+		if(pass_args)
+			re += ',' + pass_args;
+		re += ');';
+		return re;
+	});
+	return {
+		name    : "Esy Timer",
+		version : "0.0.1",
+		author  : "Slye Development Team"
+	};
+}
+module.exports  = timer;
