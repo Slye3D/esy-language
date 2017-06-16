8f5f8532fe458bf08e0aaf48f05d011b6d20b459
Alireza Ghadimi <alirezaghadimy@gmail.com>
2017-06-16 07:54:08 +0430

    Allow to check for more than one environment

--- modules/env/index.js
+++ modules/env/index.js
@@ -15,18 +15,27 @@
  */
 function env(esy) {
 	esy.configs.def('environment', []);
-	esy.block(/^env\s*(?:("|'|`)(?=[A-z]+\1))?(\w+)\1$/ig, (matches, block, parent, offset) => {
-		var env = matches[2];
+	esy.block(/^env\s*((?:(?:("|'|`)(?=[A-z]+\2))?(\w+)\2(?:\s*,\s*(?=.+))?)+)$/ig, (matches, block, parent, offset) => {
+		var env = matches[1].split(",");
 		var envs = esy.configs.get('environment');
 		if (typeof envs !== 'object' || !envs.indexOf)
 			envs = [];
+		var match = false;
+		for (var e of env) {
+			if (['"', "'", '`'].indexOf(e[0]) > -1)
+				e = e.substr(1, e.length - 2);
+			if (envs.indexOf(e) > -1) {
+				match = true;
+				break;
+			}
+		}
 		var next = offset.index + 1;
 		var el = false;
 		if (typeof parent[next] == 'object' && parent[next].head == 'else') {
 			offset.index++;
 			el = true;
 		}
-		if (envs.indexOf(env) > -1) {
+		if (match) {
 			return esy.compile(block.body);
 		} else if (el) {
 			return esy.compile(parent[next].body);
