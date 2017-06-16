07aac16c6ba2ddff754b84774c05e647f162d50c
Alireza Ghadimi <alirezaghadimy@gmail.com>
2017-06-16 09:17:01 +0430

    Accept multiple files & remove --out option

--- bin/commands/compile.js
+++ bin/commands/compile.js
@@ -11,15 +11,10 @@
 
 var PrettyError = require('pretty-error');
 
-exports.command = 'compile <file>';
+exports.command = 'compile <files..>';
 exports.desc = 'Compile Esy file to JavaScript';
 exports.builder = function (yargs) {
 	yargs
-		.option('out', {
-			describe: 'Output file descriptor',
-			type: 'string',
-			alias: 'o'
-		})
 		.option('save', {
 			describe: 'Save file in same directory',
 			default: false,
@@ -30,35 +25,32 @@ exports.builder = function (yargs) {
 			default: false,
 			alias: 't'
 		});
-		// .option('yes', {
-		// 	describe: 'Replace file even if file names are the same, otherwise user should confirm rewrite',
-		// 	default: false,
-		// 	alias: 'y'
-		// })
 };
 exports.handler = function (argv) {
-	const   fs      = require('fs'),
-			esy     = require('../loader')(argv);
-			path    = require('path');
-	var data    = fs.readFileSync(argv.file).toString();
-	var tree    = esy.tree(data);
-	var js;
-	if(argv.tree){
-		js  = JSON.stringify(tree, null, 4);
-	}else {
-		js  = esy.compile(tree);
+	const fs = require('fs'),
+		esy = require('../loader')(argv);
+	if (argv.tree && argv.files.length > 1)
+		return console.error("Error: Can not make tree for multiple files");
+	var js = '';
+	for (var file of argv.files) {
+		var data = fs.readFileSync(file).toString();
+		var tree = esy.tree(data);
+		if (argv.tree) {
+			js = JSON.stringify(tree, null, 4);
+		} else {
+			js += esy.compile(tree);
+		}
 	}
-	if(argv.out){
-		fs.writeFileSync(path.join(process.cwd(), argv.out), js)
-	}else {
-	 	if(argv.save){
-		    var file = path.join(process.cwd(), argv.file.substr(0, argv.file.lastIndexOf(".")) + '.esy.' + (argv.tree ? 'json' : 'js'));
-		    if (typeof argv.save == 'string')
-			    file = argv.save;
-		    fs.writeFileSync(file, js);
-		    console.log(`Output saved to \`${file}\``);
-	    }else {
-		    console.log(js)
-	    }
+	if (argv.save) {
+		var to = 'esy.js';
+		if (argv.files.length == 1) {
+			to = argv.files[0].substr(0, argv.files[0].lastIndexOf(".")) + '.' + (argv.tree ? 'json' : 'js');
+		}
+		if (typeof argv.save == 'string')
+			to = argv.save;
+		fs.writeFileSync(to, js);
+		console.log(`Output saved to \`${to}\``);
+	} else {
+		console.log(js)
 	}
 };
