a54053a63ab604a33cd635400fce2e4575c3f8d6
Alireza Ghadimi <alirezaghadimy@gmail.com>
2017-06-20 17:57:59 +0430

    Check for ES punctuators

---
+++ src/libs/characters/punctuator.js
@@ -0,0 +1,35 @@
+/**
+ *    _____ __
+ *   / ___// /_  _____
+ *   \__ \/ / / / / _ \
+ *  ___/ / / /_/ /  __/
+ * /____/_/\__, /\___/
+ *       /____/
+ *       Copyright 2017 Slye Development Team. All Rights Reserved.
+ *       Licence: MIT License
+ */
+
+var punctuators = "{ ( ) [ ] . ... ; , < > <= >= == != === !== + ‐ * % ++ ‐‐ << >> >>> & | ^ ! ~ && || ? : = += ‐= *= %= <<= >>= >>>= &= |= ^= => ** **= / /= }".split(' ');
+punctuators.map(c => c.trim());
+punctuators.sort((a,b) => {
+	return b.length - a.length;
+});
+
+/**
+ * Returns true if input character is a punctuator
+ * @param char
+ * @return {boolean}
+ */
+function IsPunctuator(char) {
+	return punctuators.indexOf(char) > -1;
+}
+
+IsPunctuator.chars  = punctuators;
+IsPunctuator.regex  = '(' + punctuators.join('|') + ')';
+IsPunctuator.endsWith   = string => {
+	return (new RegExp(IsPunctuator.regex + '$', 'g')).test(string)
+};
+IsPunctuator.startsWith = string => {
+	return (new RegExp('^' + IsPunctuator.regex, 'g')).test(string)
+};
+module.exports  = IsPunctuator;
