9a41b5a8ab46a848c90896dfb9357d357c21a34f
Alireza Ghadimi <alirezaghadimy@gmail.com>
2017-06-23 19:59:03 +0430

    Test break expression

--- tests/ASI/break.js
+++ tests/ASI/break.js
@@ -8,3 +8,50 @@
  *       Copyright 2017 Slye Development Team. All Rights Reserved.
  *       Licence: MIT License
  */
+
+const {compile, safeEval}   = require('../tools');
+
+module.exports.$break = function (assert) {
+	var code    = `
+re = 0;
+a:
+for(var i = 1; i < 5;i++){
+	for(var j = 0;j < 10;j++){
+		re += i*j;
+		if(j == 7){
+			break a;
+		}
+	}
+}
+re
+	`;
+	try {
+		var esy = compile(code);
+		assert(safeEval(esy) == safeEval(code));
+	}catch (e){
+		assert(false);
+	}
+};
+
+module.exports.$break1 = function (assert) {
+	var code    = `
+re = 0;
+a:
+for(var i = 1; i < 5;i++){
+	for(var j = 0;j < 10;j++){
+		re += i*j;
+		if(j == 7){
+			break 
+			a;
+		}
+	}
+}
+re
+	`;
+	try {
+		var esy = compile(code);
+		assert(safeEval(esy) == safeEval(code));
+	}catch (e){
+		assert(false);
+	}
+};
