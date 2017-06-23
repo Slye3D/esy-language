d9440ea1a907513e84bf117e66a8376188ef76b3
Alireza Ghadimi <alirezaghadimy@gmail.com>
2017-06-23 20:48:49 +0430

    Test for loop

--- tests/loops/for.js
+++ tests/loops/for.js
@@ -8,3 +8,56 @@
  *       Copyright 2017 Slye Development Team. All Rights Reserved.
  *       Licence: MIT License
  */
+
+const {compile}   = require('../tools');
+
+exports.$simple = function (assert) {
+	var j   = 0;
+	var code    = `
+	for(var i = 0;i < 10;i++){
+		j++;
+	}	
+	`;
+	try {
+		var esy = compile(code);
+		eval(esy);
+		assert(j == 10);
+	}catch (e){
+		assert(false);
+	}
+};
+
+exports.$WithoutFirstPart   = function (assert) {
+	var j   = 0;
+	var i   = 5;
+	var code    = `
+	for(;i < 10;i++){
+		j++;
+	}	
+	`;
+	try {
+		var esy = compile(code);
+		eval(esy);
+		assert(j == 5);
+	}catch (e){
+		assert(false);
+	}
+};
+
+exports.$WithoutLastPart   = function (assert) {
+	var j   = 0;
+	var code    = `
+	for(i = 0;i < 10;){
+		j++;
+		i += 2;
+	}	
+	`;
+	try {
+		var esy = compile(code);
+		eval(esy);
+		assert(j == 5);
+	}catch (e){
+		assert(false);
+	}
+};
+
