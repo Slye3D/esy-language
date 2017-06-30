cca4ffa29692161c540426f48dcfc07d4dd8e086
Alireza Ghadimi <alirezaghadimy@gmail.com>
2017-07-01 03:26:16 +0430

    Support for Angular.js

--- modules/angular/index.js
+++ modules/angular/index.js
@@ -8,3 +8,32 @@
  *       Copyright 2017 Slye Development Team. All Rights Reserved.
  *       Licence: MIT License
  */
+
+/**
+ * Support for angular.js
+ * This version (0.0.1) supports:
+ *  config
+ *  controller
+ * @param esy
+ */
+function angular(esy) {
+	let {block, compile} = esy;
+
+	const regex = /(\$[$A-Z_0-9]*)\.(config|controller)\s*(?:<(.+)>\s*)?\(((?:(?:\s*[$A-Z_][$A-Z_0-9]*\s*)(?:,(?=\s*[$A-Z_]+))?)+)?\)$/ig;
+
+	block(regex, (matches, {body, head}) => {
+		let [, app, func, param1, modules] = matches;
+		body = compile(body);
+		param1 = (param1 ? param1.length : 0) > 0 ? param1 + ', ' : '';
+		return `${app}.${func}(${param1}[${modules}, function(${modules}){
+			${body}
+		}])`;
+	});
+
+	return {
+		name    : "Esy Angular",
+		version : "0.0.1",
+		author  : "Slye Development Team"
+	};
+}
+module.exports = angular;
