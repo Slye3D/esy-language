87d1b20c0c91995f751767b1290d0f65d2b51d69
Alireza Ghadimi <alirezaghadimy@gmail.com>
2017-07-09 15:03:44 +0430

    Nested event listener

---
+++ docs/04-blocks/13-events.md
@@ -0,0 +1,7 @@
+# Events
+Syntax:
+```
+#var[<event>](args...){
+
+}
+```
--- modules/events/index.js
+++ modules/events/index.js
@@ -41,19 +41,97 @@ function events(esy) {
 
 	esy.block(pattern, (matches, block) => {
 		let variable    = matches[1],
-			event       = matches[2] || '',
-			args        = matches[3] ? ',' +matches[3] : '',
+			event       = matches[2] ? matches[2] + ',' : '',
+			args        = matches[3] || '',
 			evFunc      = matches[4] ? '.' + matches[4] : '.on',
 			body        = compile(block.body);
 
-		return `${variable}${evFunc}(${event}${args}){
+		return `${variable}${evFunc}(${event}function(${args}){
 			${body}
-		}`
+		})`
+	});
+
+	// Nested event listener
+
+	let pattern2    = new regex(
+		regex.beginning(),
+		regex.text('#'),
+
+		// Var name
+		regex.identifier().capture(),
+
+		// Default `evFunc`
+		regex.group(
+			regex.text('<'),
+			regex.callParameters().capture(),
+			regex.text('>')
+		).optional(),
+
+		regex.end()
+	).autoSpace().toObj();
+
+	let pattern3    = new regex(
+		regex.beginning(),
+
+		regex.group(
+			regex.text('<'),
+			regex.callParameters().capture(),
+			regex.text('>')
+		).optional(),
+
+		regex.text('('),
+		regex.arguments().optional().capture(),
+		regex.text(')'),
+
+		regex.group(
+			regex.text('.'),
+			regex.identifier().capture()
+		).optional(),
+
+		regex.end()
+	).autoSpace().toObj();
+
+
+	esy.block.self(pattern3);
+
+	esy.block(pattern2, (matches, {body}) => {
+		let variable    = matches[1],
+			defEvFunc   = matches[2] ? matches[2] : 'on',
+			re          = '';
+
+		body.forEach(value => {
+			if(typeof value !== 'object')
+				throw new Error('Wrong nested event listener.');
+			let {head, body}    = value,
+				e               = new RegExp(pattern3);
+			if(e.test(head)){
+				let match = e.exec(head);
+				while(match === null){
+					match = e.exec(head);
+				}
+
+				let event       = match[1] ? match[1] + ',' : '',
+					args        = match[2] || '',
+					evFunc      = match[3] ? '.' + match[3] : '.' + defEvFunc,
+					b           = compile(body);
+
+				re  +=
+`${variable}${evFunc}(${event}function(${args}){
+	${b}
+})
+`
+
+			}else {
+				throw new Error('Wrong nested event listener.');
+			}
+		});
+
+		return re;
 	});
 
 	return {
 		name: "Esy Events",
-		version: "0.0.1",
+		version: "0.0.2",
 		author: "Slye Development Team"
 	};
 }
