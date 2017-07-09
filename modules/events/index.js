08ddf0c6119478ca3920e632fca29c8d19e7c68d
Alireza Ghadimi <alirezaghadimy@gmail.com>
2017-07-09 12:04:11 +0430

    Events modules

--- modules/events/index.js
+++ modules/events/index.js
@@ -8,3 +8,53 @@
  *       Copyright 2017 Slye Development Team. All Rights Reserved.
  *       Licence: MIT License
  */
+
+/**
+ * Events
+ * @param esy
+ */
+function events(esy) {
+	const {regex, block, compile} = esy;
+	var pattern = new regex(
+		regex.beginning(),
+
+		regex.text('#'),
+		regex.identifier().capture(),
+
+		regex.group(
+			regex.text('<'),
+			regex.callParameters().capture(),
+			regex.text('>')
+		).optional(),
+
+		regex.text('('),
+		regex.arguments().optional().capture(),
+		regex.text(')'),
+
+		regex.group(
+			regex.text('.'),
+			regex.identifier().capture()
+		).optional(),
+
+		regex.end()
+	).autoSpace().toObj();
+
+	esy.block(pattern, (matches, block) => {
+		let variable    = matches[1],
+			event       = matches[2] || '',
+			args        = matches[3] ? ',' +matches[3] : '',
+			evFunc      = matches[4] ? '.' + matches[4] : '.on',
+			body        = compile(block.body);
+
+		return `${variable}${evFunc}(${event}${args}){
+			${body}
+		}`
+	});
+
+	return {
+		name: "Esy Events",
+		version: "0.0.1",
+		author: "Slye Development Team"
+	};
+}
+module.exports = events;
